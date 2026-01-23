788215011817632d26fcb0b61d532685f0ae60a7
arnold <arnold@jasny.net>
2026-01-23 14:05:30 +0100

    Change step def 'I see/dont see' into 'page contains/not contains'. Similar to locator contains, better for LLM mental model.

--- packages/bdd/src/steps/assert.ts
+++ packages/bdd/src/steps/assert.ts
@@ -4,10 +4,13 @@ import { Then } from './wrappers';
 
 const WAIT_TIMEOUT = 5000;
 
-export const see = Then("I {see|dont see} {locator}", async function (visible: boolean, selector: string) {
-  const el = await locator(this.page, selector);
-  await expectOrNot(el, visible).toBeVisible({ timeout: WAIT_TIMEOUT });
-});
+export const see = Then(
+  'I see that the page {contains|not contains} {locator}',
+  async function (visible: boolean, selector: string) {
+    const el = await locator(this.page, selector);
+    await expectOrNot(el, visible).toBeVisible({ timeout: WAIT_TIMEOUT });
+  },
+);
 
 export const contain = Then(
   'I see that {locator} {contains|not contains} {locator}',
