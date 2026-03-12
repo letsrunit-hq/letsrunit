b8fc1cbbc8d43bb06200bb54254c604c20d0a93a
arnold <arnold@jasny.net>
2026-03-12 16:42:04 +0100

    fix(letsrunit): Add require support files for cucumber on install.

--- packages/letsrunit/src/setup/cucumber.ts
+++ packages/letsrunit/src/setup/cucumber.ts
@@ -5,6 +5,7 @@ import { type Environment, execPm } from '../detect.js';
 const BDD_IMPORT = '@letsrunit/bdd/define';
 
 const CUCUMBER_CONFIG = `export default {
+  require: ['features/support/**/*.js'],
   worldParameters: {
     baseURL: 'http://localhost:3000',
   },
