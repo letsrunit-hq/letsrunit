f386523aef8494db6e17a86feb1bd22a9bfdd5ed
arnold <arnold@jasny.net>
2026-04-02 17:54:56 +0200

    chore: simplify store/index exports

--- packages/store/src/index.ts
+++ packages/store/src/index.ts
@@ -1,21 +1,4 @@
-export { openStore } from './db';
-export {
-  insertRun,
-  upsertFeature,
-  upsertScenario,
-  upsertStep,
-  upsertScenarioStep,
-  insertTest,
-  finaliseTest,
-  insertArtifact,
-} from './write';
-export {
-  computeStepId,
-  computeScenarioId,
-  computeFeatureId,
-  computeRuleId,
-  computeOutlineId,
-  computeExampleRowId,
-} from './ids';
-export { findLastTest, findLastPassingBaseline, findArtifacts, findLastRun } from './read';
-export type { LastRun, LastRunTest, LastRunStep } from './read';
+export * from './db';
+export * from './write';
+export * from './ids';
+export * from './read';
