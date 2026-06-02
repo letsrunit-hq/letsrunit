68e8a0e7aaf58067a6a2963f071e7251fa57d965
Arnold Daniels <arnold@jasny.net>
2026-06-02 13:59:18 +0200

    chore: update deps and fix compat tests

--- compat/react/playwright.config.ts
+++ compat/react/playwright.config.ts
@@ -2,11 +2,12 @@ import { defineConfig } from '@playwright/experimental-ct-react';
 
 export default defineConfig({
   testDir: './tests',
+  outputDir: './test-results',
 
   timeout: 30_000,
 
   use: {
-    //ctPort: 3100,
+    ctPort: 3100,
     viewport: { width: 1280, height: 800 },
   },
 
